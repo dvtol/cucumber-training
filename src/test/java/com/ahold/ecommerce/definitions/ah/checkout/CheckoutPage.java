43d7ac8e594e6bca199a6d6c3fce6513fefa48bf
Danny Van Tol <danny.tol.van@ahold.com>
2017-07-11 20:39:55 +0200

    finished checkout part for elcheapo. xpaths needs to be replaced with Selenide filters to make it final

--- src/test/java/com/ahold/ecommerce/definitions/ah/checkout/CheckoutPage.java
+++ src/test/java/com/ahold/ecommerce/definitions/ah/checkout/CheckoutPage.java
@@ -1,5 +1,7 @@
 package com.ahold.ecommerce.definitions.ah.checkout;
 
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.$;
 
 import com.ahold.ecommerce.definitions._generics.BasePage;
@@ -17,6 +19,15 @@ public class CheckoutPage extends BasePage {
     @Step("Checkout selected product(s) on AH web and finish order")
     public void orderProduct() {
 
+        //TODO - replace xpath with Selenide filter option!
+
         $(testDataHook("link-guidance-to-channel")).click();
+        $(byXpath("//*[@id=\"change\"]/label[2]/span[2]")).shouldBe(visible).click();
+        $(testDataHook("timeslot-6-selectable")).click();
+        $(testDataHook("link-checkout-flow")).click();
+        $(byXpath("//*[@id=\"index_1\"]/article/a[2]")).shouldBe(visible).click();
+
+        // confirm order
+        $(byXpath("//*[@id=\"index_1\"]/article/a[2]")).click();
     }
 }
--- src/test/java/com/ahold/ecommerce/definitions/ah/ordering/OrderPage.java
+++ src/test/java/com/ahold/ecommerce/definitions/ah/ordering/OrderPage.java
@@ -23,8 +23,7 @@ public class OrderPage extends BasePage {
         $(testDataHook("link-2861")).shouldBe(Condition.visible).click();
         $(testDataHook("add_product_wi56046")).shouldBe(Condition.visible).click();
         $(testDataHook("add_product_wi31584")).shouldBe(Condition.visible).click();
-        $(testDataHook("add_product_wi55686")).shouldBe(Condition.visible).click();
-        final SelenideElement shoppingbasket = $(testDataHook("navigation-shoppinglist")).shouldHave(text("3"));
+        final SelenideElement shoppingbasket = $(testDataHook("navigation-shoppinglist")).shouldHave(text("2"));
         shoppingbasket.click();
 
     }
