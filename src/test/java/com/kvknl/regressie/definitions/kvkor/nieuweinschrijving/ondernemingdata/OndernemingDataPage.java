56b57c928669f3ff74065645c8fc015e6e9d5e64
Danny van Tol <danny.van.tol@kvk.nl>
2017-12-20 13:58:11 +0100

    Ondeneming data nieuwe inschrijving BV afgemaakt en referentie data aangevuld

---
+++ src/test/java/com/kvknl/regressie/definitions/kvkor/nieuweinschrijving/ondernemingdata/OndernemingDataPage.java
@@ -0,0 +1,43 @@
+package com.kvknl.regressie.definitions.kvkor.nieuweinschrijving.ondernemingdata;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import com.kvknl.regressie.data._JsonData;
+import com.kvknl.regressie.definitions._generics.BasePage;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class OndernemingDataPage extends BasePage {
+
+    public OndernemingDataPage(WebDriver webDriver) {
+        super(webDriver);
+        WebDriverRunner.setWebDriver(webDriver);
+    }
+
+    // JSON obj for GUI data
+    private _JsonData testdata = new _JsonData();
+
+    @Step("het invoeren van de gegevens van de orderneming waaronder de vestiging(en)")
+    public void OpvoerenVestiging() {
+
+        $(By.id("Datum_ingang")).setValue(testdata.JsonData("kvk_orn_nieuwe_inschrijving", "datum_ingang_vestiging"));
+        $(By.id("Postcode")).setValue(testdata.JsonData("kvk_orn_nieuwe_inschrijving", "postcode_vestiging"));
+        $(By.id("Huisnummer")).setValue(testdata.JsonData("kvk_orn_nieuwe_inschrijving", "huisnummer_vestiging"));
+        $(By.id("Activiteitomschrijving")).waitUntil(Condition.visible, 5).setValue(testdata.JsonData("kvk_orn_nieuwe_inschrijving", "activiteitomschrijving_vestiging"));
+        // TODO - fix company trade name
+        $(By.id("deletableHandelsnaam_1_*_")).setValue(testdata.JsonData("kvk_orn_nieuwe_inschrijving", "handelsnaam_vestiging"));
+        $(By.id("deletableHandelsnaam_1_*_")).setValue(testdata.JsonData("kvk_orn_nieuwe_inschrijving", "handelsnaam_vestiging"));
+        $(By.id("Hoeveel_personen_werken_er_fulltime_(15_uur_of_meer_per_week)_in_de_onderneming?")).setValue(testdata.JsonData("kvk_orn_nieuwe_inschrijving", "werkzame_pers_fulltime_vestiging"));
+        $(By.id("Hoeveel_personen_werken_er_parttime_(minder_dan_15_uur_per_week)_in_de_onderneming?")).setValue(testdata.JsonData("kvk_orn_nieuwe_inschrijving", "werkzame_pers_parttime_vestiging"));
+        $(By.id("Volgende")).click();
+        // geen functionarissen aanwezig
+        $(By.id("Volgende")).click();
+        // geen gemachtigden aanwezig
+        $(By.id("Volgende")).click();
+        // geen bijlage(n)
+        $(By.id("Volgende")).click();
+    }
+}
---
+++ src/test/java/com/kvknl/regressie/definitions/kvkor/nieuweinschrijving/referentiedata/ReferentieDataPage.java
@@ -0,0 +1,34 @@
+package com.kvknl.regressie.definitions.kvkor.nieuweinschrijving.referentiedata;
+
+import com.codeborne.selenide.WebDriverRunner;
+import com.kvknl.regressie.data._JsonData;
+import com.kvknl.regressie.definitions._generics.BasePage;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class ReferentieDataPage extends BasePage {
+
+    public ReferentieDataPage(WebDriver webDriver) {
+        super(webDriver);
+        WebDriverRunner.setWebDriver(webDriver);
+    }
+
+    // JSON obj for GUI data
+    private _JsonData testdata = new _JsonData();
+
+    @Step("het invullen van de referentie informatie wat betreft de onderneming")
+    public void VulReferentieInformatie() {
+
+        $(By.id("Naam_contactpersoon")).setValue(testdata.JsonData("kvk_orn_nieuwe_inschrijving", "naam_contactpersoon_referent"));
+        $(By.id("E-mailadres_contactpersoon")).setValue(testdata.JsonData("kvk_orn_nieuwe_inschrijving", "email_contactpersoon_referent"));
+
+        try {
+            Thread.sleep(12000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
