d19f6c0cd84bc348954356abaff8a52b4636b3f7
Danny van Tol <danny.van.tol@kvk.nl>
2018-01-04 12:05:30 +0100

    required attachment DOM string as global variable

--- src/test/java/com/kvknl/regressie/definitions/kvkor/newregistration/fileattachment/FileAttachmentPage.java
+++ src/test/java/com/kvknl/regressie/definitions/kvkor/newregistration/fileattachment/FileAttachmentPage.java
@@ -7,10 +7,13 @@ import com.kvknl.regressie.definitions.generic.attachments.AttachmentPage;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class FileAttachmentPage extends BasePage {
+
+    private final String requiredfile = "//*[@id='Bijlage_1']/input";
 
     public FileAttachmentPage(WebDriver webDriver) {
         super(webDriver);
@@ -21,21 +24,11 @@ public class FileAttachmentPage extends BasePage {
     public void addFileAttachment() {
 
         dropdownSelectByValue((By.id("Type_bijlage")), "Datacard Test Tester Getest van van Dongen (verplicht)");
-        findElement(By.xpath("//*[@id='Bijlage_1']/input")).sendKeys(AttachmentPage.loadAttachment("Datacard.pdf"));
+        findElement(By.xpath(requiredfile)).sendKeys(AttachmentPage.loadAttachment("Datacard.pdf"));
         buttonClick(By.xpath("/html/body/div[2]/div[4]/div/div[2]/div/div[5]/button"));
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         dropdownSelectByValue((By.id("Type_bijlage")), "Akte van oprichting (verplicht)");
-        findElement(By.xpath("//*[@id='Bijlage_1']/input")).sendKeys(AttachmentPage.loadAttachment("Akte van oprichting.pdf"));
+        findElement(By.xpath(requiredfile)).sendKeys(AttachmentPage.loadAttachment("Akte van oprichting.pdf"));
         buttonClick(By.xpath("/html/body/div[2]/div[4]/div/div[2]/div/div[5]/button"));
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         $(By.id("Opslaan")).click();
         $(By.id("Volgende")).click();
     }
