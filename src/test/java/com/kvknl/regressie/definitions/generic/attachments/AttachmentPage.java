ab57ccaa14e4f3f22f76c76e08f532902b286798
Danny van Tol <danny.van.tol@kvk.nl>
2018-01-18 14:27:00 +0100

    removed unused imports from attachmentpage class

--- src/test/java/com/kvknl/regressie/definitions/generic/attachments/AttachmentPage.java
+++ src/test/java/com/kvknl/regressie/definitions/generic/attachments/AttachmentPage.java
@@ -1,17 +1,13 @@
 package com.kvknl.regressie.definitions.generic.attachments;
 
 import com.codeborne.selenide.WebDriverRunner;
-import com.kvknl.regressie.data._JsonData;
 import com.kvknl.regressie.definitions.generic.BasePage;
-import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
-
-import static com.codeborne.selenide.Selenide.$;
 
 public class AttachmentPage extends BasePage {
 
