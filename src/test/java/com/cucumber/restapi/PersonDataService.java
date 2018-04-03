7a3231436bb198b3163341427883ca0f96220a51
Glenn Dasai <glenn.dasai@gmail.com>
2018-04-03 20:47:05 +0200

    Added restapi verifytest

--- src/test/java/com/cucumber/restapi/PersonDataService.java
+++ src/test/java/com/cucumber/restapi/PersonDataService.java
@@ -1,15 +1,16 @@
 package com.cucumber.restapi;
 
 import com.cucumber.definitions.stepdefs.BaseStepDef;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import cucumber.api.java.en.And;
-
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 
 
 public class PersonDataService extends BaseStepDef {
@@ -22,11 +23,12 @@ public class PersonDataService extends BaseStepDef {
 
     @Given("a person with the name (.*)")
     public void aPersonWithName(String name){
-        request = given().param("q", "name:" + name);
+//        request = given().param("q", "name:" + name);
+        request = given();
     }
 
     @And("a the user is retrieved data in JSON format")
-    public void retreiveXmlPersonData(){
+    public void retrieveJsonPersonData(){
         response = request.when().get(API_ENDPOINT);
         System.out.println("response: " + response.prettyPrint());
     }
@@ -34,5 +36,20 @@ public class PersonDataService extends BaseStepDef {
     @Then("the status code is (\\d+)")
     public void verify_status_code(int statusCode) {
         json = response.then().statusCode(statusCode);
+    }
+
+    @Then("^verify that element name has value \"([^\"]*)\"$")
+    public void verifyThatElementNameHasValue(String name) {
+        response.then().assertThat().body("name", equalTo(name));
+    }
+
+    @Then("^verify that element age has value (\\d+)$")
+    public void verifyThatElementAgeHasValue(int age) {
+        response.then().assertThat().body("age", equalTo(age));
+    }
+
+    @Then("^verify that person has (\\d+) cars$")
+    public void verifyThatPersonHasCars(int cars) {
+        response.then().assertThat().body("cars.car", hasSize(cars));
     }
 }
--- src/test/resources/features/restapitest/restapitest.feature
+++ src/test/resources/features/restapitest/restapitest.feature
@@ -4,5 +4,8 @@ Feature: Get (personal) data via rest api calls
 
   Scenario: User calls web service to get a person by his/her name
     Given a person with the name John
-    And a the user is retrieved data in JSON format
+    When a the user is retrieved data in JSON format
     Then the status code is 200
+    Then verify that element name has value "John"
+    Then verify that element age has value 30
+    Then verify that person has 3 cars
